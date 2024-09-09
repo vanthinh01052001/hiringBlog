50ec08d7cda45905bb580e5cd7280e4a6c1eda5c
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-09-09 17:54:16 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import Hero from '@/components/Hero.vue';
+  import HomeCard from '@/components/HomeCard.vue';
+  import JobListings from '@/components/JobListings.vue';
+</script>
+<template>
+  <Hero/>
+  <HomeCard />
+  <JobListings :limit="3" :show-button="true"/>
+</template>
